a0e247d3af01077535103bcbe6f309d7714ccd08
Darragh Murphy <dmurphy@servuo.com>
2023-07-02 19:01:37 -0700

    Add stats to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: "Jungle", password: "book"
   def show
+    @products = Product.count
+    @categories = Category.count
+    @orders = Order.count
   end
 end
